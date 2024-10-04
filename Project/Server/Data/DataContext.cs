ed4b07e781ddc3b9a3014400bdbd5e144f1a2b38
ArsBackend7 <ars.ict.developer@gmail.com>
2024-10-04 16:33:39 +0600

    NCA-2

--- Project/Server/Data/DataContext.cs
+++ Project/Server/Data/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Server.Entities.Models;
 
 namespace Server.Data;
 
@@ -6,5 +7,25 @@ public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
+    }
+
+    public DbSet<AppUser> AppUsers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<AppUser>().HasData(
+            new AppUser { Id = 1, UserName = "john_doe" },
+            new AppUser { Id = 2, UserName = "jane_smith" },
+            new AppUser { Id = 3, UserName = "michael_brown" },
+            new AppUser { Id = 4, UserName = "emily_jones" },
+            new AppUser { Id = 5, UserName = "david_wilson" },
+            new AppUser { Id = 6, UserName = "sarah_davis" },
+            new AppUser { Id = 7, UserName = "chris_miller" },
+            new AppUser { Id = 8, UserName = "laura_garcia" },
+            new AppUser { Id = 9, UserName = "daniel_lee" },
+            new AppUser { Id = 10, UserName = "olivia_martin" }
+        );
     }
 }
---
+++ Project/Server/Migrations/20241004103237_AppUserModel.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AppUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUsers", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AppUsers",
+                columns: new[] { "Id", "UserName" },
+                values: new object[,]
+                {
+                    { 1, "john_doe" },
+                    { 2, "jane_smith" },
+                    { 3, "michael_brown" },
+                    { 4, "emily_jones" },
+                    { 5, "david_wilson" },
+                    { 6, "sarah_davis" },
+                    { 7, "chris_miller" },
+                    { 8, "laura_garcia" },
+                    { 9, "daniel_lee" },
+                    { 10, "olivia_martin" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppUsers");
+        }
+    }
+}
